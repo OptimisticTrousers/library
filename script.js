cc1477873cfc72e7c3fc47f849b6e252e2673e86
OptimisticTrousers <tony.isern@yahoo.com>
2022-05-25 22:42:03 -0400

    Adding submit functionality to button

--- script.js
+++ script.js
@@ -2,6 +2,27 @@ const addBookButton = document.querySelector('button.add-new-book');
 
 const form = document.querySelector('form');
 
+//const author = form.elements['author'];
+const title = form.elements['title'];
+const pages = form.elements['pages'];
+const userHasRead = form.elements['has_read'];
+
+const author = document.querySelector('#author');
+
+const submitButton = document.querySelector('button.submit');
+
+
+
+form.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+})
+
+submitButton.addEventListener('click', () => {
+
+    console.log(author.value)
+})
+
 addBookButton.addEventListener('click', () => {
 
     //form.style.display = "none";
